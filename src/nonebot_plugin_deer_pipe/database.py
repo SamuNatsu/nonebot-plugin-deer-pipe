4c846324405aa8b9203a02df304446aecc0a39d0
SNRainiar <rainiar@foxmail.com>
2024-08-12 11:58:30 +0800

    feat: use integer for mask

--- src/nonebot_plugin_deer_pipe/database.py
+++ src/nonebot_plugin_deer_pipe/database.py
@@ -12,7 +12,7 @@ class User(SQLModel, table=True):
   user_id: str = Field(primary_key=True)
   year: int
   month: int
-  mask: str    = "0"
+  mask: int    = 0
 
 # Async engine
 engin: AsyncEngine = create_async_engine(DATABASE_URL)
@@ -20,21 +20,19 @@ initialized: bool = False
 
 # Attendance
 def get_seq(mask: int) -> list[int]:
-  return list(
-    map(
-      lambda x: x[0] + 1,
-      filter(
-        lambda x: x[1] == '1',
-        enumerate(bin(mask)[2:][::-1])
-      )
-    )
-  )
+  ret: list[int] = []
+  while mask > 0:
+    lb: int = mask & -mask
+    mask -= lb
+    ret.append(lb.bit_length())
+
+  return ret
 
 async def attend(now: datetime, user_id: str) -> tuple[bool, Sequence[int]]:
   if not initialized:
     async with engin.begin() as conn:
       await conn.run_sync(SQLModel.metadata.create_all)
-  
+
   async with AsyncSession(engin) as session:
     user: User | None = (
       await session.exec(select(User).where(User.user_id == user_id))
@@ -42,15 +40,14 @@ async def attend(now: datetime, user_id: str) -> tuple[bool, Sequence[int]]:
 
     if user == None or user.year != now.year or user.month != now.month:
       user = User(user_id=user_id, year=now.year, month=now.month)
-    
-    mask: int = int(user.mask)
-    if (mask >> (now.day - 1)) & 1 == 1:
-      return (False, get_seq(mask))
+
+    if (user.mask >> (now.day - 1)) & 1 == 1:
+      return (False, get_seq(user.mask))
     else:
-      mask |= (1 << (now.day - 1))
-      user.mask = str(mask)
+      user.mask |= (1 << (now.day - 1))
 
       session.add(user)
       await session.commit()
+      await session.refresh(user)
 
-      return (True, get_seq(mask))
+      return (True, get_seq(user.mask))
