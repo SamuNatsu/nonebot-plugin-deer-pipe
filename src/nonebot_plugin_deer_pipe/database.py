1f2623911b2e0d028d1262c6568b745474f3f443
SNRainiar <rainiar@foxmail.com>
2024-09-01 11:18:44 +0800

    fix: fail to reattend

--- src/nonebot_plugin_deer_pipe/database.py
+++ src/nonebot_plugin_deer_pipe/database.py
@@ -22,45 +22,55 @@ class UserDeer(SQLModel, table=True):
   count: int    = 0
 
 # Async engine
-engin: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
+engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
 initialized: bool = False
 
-# Fetch user deer
-async def fetch_user_deer(now: datetime, user_id: str) -> Sequence[UserDeer]:
+# Initialize engine
+async def init_engine() -> None:
   global initialized
   if not initialized:
     initialized = True
-    async with engin.begin() as conn:
+    async with engine.begin() as conn:
       await conn.run_sync(SQLModel.metadata.create_all)
 
-  async with AsyncSession(engin) as session:
-    user: User | None = (
-      await session.exec(select(User).where(User.id == user_id))
-    ).one_or_none()
+# Fetch user deer
+async def fetch_user_deer(
+  session: AsyncSession,
+  now: datetime,
+  user_id: str
+) -> Sequence[UserDeer]:
+  user: User | None = (
+    await session.exec(select(User).where(User.id == user_id))
+  ).one_or_none()
 
-    user_deer: Sequence[UserDeer] = []
-    if user == None:
-      user = User(id=user_id, year=now.year, month=now.month)
-      session.add(user)
-    elif user.year != now.year or user.month != now.month:
-      await session.exec(delete(UserDeer).where(UserDeer.user_id == user_id))
-      user.year = now.year
-      user.month = now.month
-      session.add(user)
-    else:
-      user_deer = (
-        await session.exec(select(UserDeer).where(UserDeer.user_id == user_id))
-      ).all()
+  user_deer: Sequence[UserDeer] = []
+  if user == None:
+    user = User(id=user_id, year=now.year, month=now.month)
+    session.add(user)
+  elif user.year != now.year or user.month != now.month:
+    await session.exec(delete(UserDeer).where(UserDeer.user_id == user_id))
+    user.year = now.year
+    user.month = now.month
+    session.add(user)
+  else:
+    user_deer = (
+      await session.exec(select(UserDeer).where(UserDeer.user_id == user_id))
+    ).all()
 
-  await session.commit()
   return user_deer
 
 # Attendance
 async def attend(now: datetime, user_id: str) -> dict[int, int]:
-  user_deer: Sequence[UserDeer] = await fetch_user_deer(now, user_id)
-  deer_map: dict[int, int] = dict([(i.day, i.count) for i in user_deer])
+  await init_engine()
 
-  async with AsyncSession(engin) as session:
+  async with AsyncSession(engine) as session:
+    user_deer: Sequence[UserDeer] = await fetch_user_deer(
+      session,
+      now,
+      user_id
+    )
+    deer_map: dict[int, int] = dict([(i.day, i.count) for i in user_deer])
+
     if now.day in deer_map:
       deer_map[now.day] += 1
       current: UserDeer = next(filter(lambda x: x.day == now.day, user_deer))
@@ -78,15 +88,21 @@ async def reattend(
   day: int,
   user_id: str
 ) -> tuple[bool, dict[int, int]]:
-  user_deer: Sequence[UserDeer] = await fetch_user_deer(now, user_id)
-  deer_map: dict[int, int] = dict([(i.day, i.count) for i in user_deer])
+  await init_engine()
 
-  async with AsyncSession(engin) as session:
-    if day in deer_map:
-      await session.commit()
-      return (False, deer_map)
+  async with AsyncSession(engine) as session:
+    user_deer: Sequence[UserDeer] = await fetch_user_deer(
+      session,
+      now,
+      user_id
+    )
+    deer_map: dict[int, int] = dict([(i.day, i.count) for i in user_deer])
+    ok: bool = False
 
-    deer_map[day] = 1
-    session.add(UserDeer(user_id=user_id, day=day, count=1))
+    if day not in deer_map:
+      deer_map[day] = 1
+      session.add(UserDeer(user_id=user_id, day=day, count=1))
+      ok = True
+
     await session.commit()
-    return (True, deer_map)
+    return (ok, deer_map)
